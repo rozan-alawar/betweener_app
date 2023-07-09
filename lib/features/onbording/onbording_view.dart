ae22af7b2a37c21af15154d024e59d37cf67e5a7
Asil Awad <aseelawad8@hotmail.com>
2023-07-09 15:24:42 +0300

    Initial Commit

---
+++ lib/features/onbording/onbording_view.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../core/util/assets.dart';
+import '../../core/util/constants.dart';
+import '../../core/widgets/secondary_button_widget.dart';
+import '../auth/login_view.dart';
+
+class OnBoardingView extends StatelessWidget {
+  static String id = '/onBoardingView';
+
+  const OnBoardingView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(32.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            const Spacer(),
+            SvgPicture.asset(AssetsData.onBoardingImage),
+            const Text(
+              'Just one Scan for everything',
+              style: TextStyle(
+                  fontSize: 16,
+                  color: kPrimaryColor,
+                  fontWeight: FontWeight.w600),
+            ),
+            const Spacer(),
+            SecondaryButtonWidget(
+              text: 'Get Started',
+              width: double.infinity,
+              onTap: () {
+                Navigator.pushNamed(context, LoginView.id);
+              },
+            ),
+            const Spacer()
+          ],
+        ),
+      ),
+    );
+  }
+}
